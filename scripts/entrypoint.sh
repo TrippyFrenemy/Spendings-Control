02e24b4275eb7a19b917b5f91676feab79635443
Dmytro Avrushchenko <trippyfren@gmail.com>
2025-08-31 23:58:26 +0300

    fix(entrypoint): add alembic revision command before migrations

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,6 +3,9 @@
 echo "⏳ Waiting for DB to be ready..."
 sleep 2
 
+alembic revision
+alembic revision --autogenerate -m "Auto migration"
+
 echo "⚙️ Running Alembic migrations..."
 alembic upgrade head
 
